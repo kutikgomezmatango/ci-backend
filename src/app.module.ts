c479eea813042129c7f546487ff659acb46990be
kutikgomezmatango <kutik.qdev@gmail.com>
2025-08-10 18:31:10 -0500

    feat(prisma): install and db postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }), // .env disponible vía ConfigService
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hola Mundo! Bienvenido a tu API NestJS. 2023';
+    return 'API NestJS. 2023';
   }
 }
